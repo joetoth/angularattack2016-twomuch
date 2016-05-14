037b6bdaefd32c3cfe403f638b6cea9ae6fc6838
Sarah Edkins <edkins.sarah@gmail.com>
2016-05-13 20:32:23 -0400

    my first component - websites iframe

--- app/app.component.js
+++ app/app.component.js
@@ -2,9 +2,21 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>AngularAttack 2016</h1><p>Your project runs!</p>'
+      template: '<h1>AngularAttack 2016</h1><p>Your project runs - like yeah it does!</p>'
     })
     .Class({
       constructor: function() {}
     });
+
+    app.websiteComponent =
+      ng.core.Component({
+        selector: 'website',
+        templateUrl: 'app/templates/website.html'
+      })
+      .Class({
+        constructor: function() {
+          this.title = "This is a website!";
+          this.websiteURL = "http://www.w3schools.com/tags/tag_iframe.asp";
+        }
+      })
 })(window.app || (window.app = {}));
--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 (function(app) {
   document.addEventListener('DOMContentLoaded', function() {
     ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+    ng.platformBrowserDynamic.bootstrap(app.websiteComponent);
   });
 })(window.app || (window.app = {}));
