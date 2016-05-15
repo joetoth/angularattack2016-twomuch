93ca7b0e0e22983677ab789ad72e62b1014888a0
Joe Toth <joetoth@gmail.com>
2016-05-15 19:45:44 -0400

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+
+var jspm = require('gulp-jspm-build');
+gulp.task("jspm_bundle", function () {
+return jspm({
+    bundleOptions: {
+        minify: true,
+        mangle: false
+    },
+    bundleSfx: true,
+    bundles: [
+        { src: './app/main.js', dst: 'main.min.js' }
+    ]
+})
+.pipe(gulp.dest('./dist/'));
+
+
+});
+
+ gulp.task("dist", function () {
+var path = require("path");
+var Builder = require('systemjs-builder');
+ 
+// optional constructor options 
+// // sets the baseURL and loads the configuration file 
+ var builder = new Builder('.', 'systemjs.config.js');
+  builder
+  .bundle('app/main.js', 'outfile.js')
+ .then(function() {
+    console.log('Build complete');
+    })
+    .catch(function(err) {
+      console.log('Build error');
+        console.log(err);
+        });
+
+ });
